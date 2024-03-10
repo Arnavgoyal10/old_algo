943395a8973abdc4242ae8faf880a25ac145fe88
Arnavgoyal10 <arnavgoyal63774@gmail.com>
2024-03-11 02:56:33 +0530

    good

--- main.py
+++ main.py
@@ -14,10 +14,14 @@ import hull_ma
 import okx
 import squeeze
 import ranged
+# trio-websocket, websocket, websocket-client, ypy-websocket 
 import obv
+# from tvDatafeed import TvDatafeed, Interval
 
-data_dict = files_interact.extract()      
-client=login.login()
+# tv = TvDatafeed()
+
+# data_dict = files_interact.extract()      
+# client=login.login()
 ohlc=['into', 'inth', 'intl', 'intc']
      
 def set_stoploss(temp, side, stoploss):
@@ -95,16 +99,21 @@ def check_trade(df):
     comparison_time = current_time.replace(hour=15, minute=00, second=0, microsecond=0)
     bull_or_bear = "none"
     stoploss = 0
+    reason = "nothing"
     
     
     if current_time < comparison_time:        
         if (df_super["STX17_3.0"].iloc[-1] != df_super["STX17_3.0"].iloc[-2]):
             supertrend_signal = True
+        else:
+            reason = "super_trend_ said no"
      
         if (supertrend_signal == True and df_range["in range"].iloc[-1] == False):
             if (df_squeeze["psi"].iloc[-1] < 80 and df_squeeze["psi"].iloc[-2] < 80 and df_squeeze["psi"].iloc[-1] < df_squeeze["psi"].iloc[-2]):
                 bull_or_bear = df_super["STX17_3.0"].iloc[-1]
                 small_signal = True
+            else:
+                reason = "Squee said no"
         
         if (df_impulse["ImpulseMACD"].iloc[-1] != 0 and small_signal == True): 
             if (bull_or_bear == "up"):
@@ -118,7 +127,9 @@ def check_trade(df):
                 impulse_waiting = True
             elif (impulse_temp1 > impulse_temp2 and abs(impulse_temp1) > 4):
                 impulse_signal = True
-            
+            else:
+                reason = "impulse said no"
+
         if (impulse_signal == True or impulse_waiting == True):
             if (bull_or_bear == "up"):
                 tsi_temp1 = (df_tsi["TSI_13_25_13"].iloc[-1]) - (df_tsi["TSIs_13_25_13"].iloc[-1])
@@ -133,6 +144,7 @@ def check_trade(df):
                 signal = True
             else:
                 impulse_waiting = False
+                reason = "tsi said no"
     
         if(impulse_waiting == True or tsi_waiting == True):
             confirmation_waiting = True    
@@ -144,9 +156,9 @@ def check_trade(df):
                 stoploss = set_stoploss(df, bull_or_bear, stoploss)
         
         if (confirmation_waiting == True):
-            signal = False
-            
-    return signal, confirmation_waiting, bull_or_bear, stoploss
+            signal = False 
+            reason = "order going in confirmation"   
+    return signal, confirmation_waiting, bull_or_bear, stoploss, reason
               
 def place_order(df, bull_or_bear, stoploss):
     time = df["time"].iloc[-1]
@@ -191,31 +203,39 @@ def check_exit(df, side, stoploss):
     return time, exit_price, order_placed, order_exit, stoploss
     
 def main():
-    
-    token = files_interact.get_token("NSE", "Nifty 50")
-    lastBusDay = datetime.datetime.now()-datetime.timedelta(days=30)
-    lastBusDay = lastBusDay.replace(hour=0, minute=0, second=0, microsecond=0)
-    # last_day = datetime.datetime.now()-datetime.timedelta(days=27)
-    # last_day = last_day.replace(hour=0, minute=0, second=0, microsecond=0)
-    ret = client.get_time_price_series(exchange="NSE", token = str(int(token)), starttime=int(lastBusDay.timestamp()), interval="5")
-    ret = pd.DataFrame.from_dict(ret)
+    # ret = tv.get_hist(symbol='NIFTY',exchange='NSE',interval=Interval.in_5_minute,n_bars=1000)
+        
+    file_path = 'Book1.xlsx'
+    ret = pd.read_excel(file_path)
     ret["time"] = pd.to_datetime(ret["time"], dayfirst=True)
-    ret.sort_values(by='time', ascending=True, inplace=True)
-    ret.reset_index(inplace=True)
     for col in ohlc:
         ret[col] = ret[col].astype(float)
+    
+    # token = files_interact.get_token("NSE", "Nifty 50")
+    # lastBusDay = datetime.datetime.now()-datetime.timedelta(days=30)
+    # lastBusDay = lastBusDay.replace(hour=0, minute=0, second=0, microsecond=0)
+    # # last_day = datetime.datetime.now()-datetime.timedelta(days=27)
+    # # last_day = last_day.replace(hour=0, minute=0, second=0, microsecond=0)
+    # ret = client.get_time_price_series(exchange="NSE", token = str(int(token)), starttime=int(lastBusDay.timestamp()), interval="5")
+    # ret = pd.DataFrame.from_dict(ret)
+    # ret["time"] = pd.to_datetime(ret["time"], dayfirst=True)
+    # ret.sort_values(by='time', ascending=True, inplace=True)
+    # ret.reset_index(inplace=True)
+    # for col in ohlc:
+    #     ret[col] = ret[col].astype(float)
+    
     temp = pd.DataFrame()
-    temp = ret.iloc[:530].copy()
-    data_columns = ['entry_time', 'entry_price', 'exit_time', 'exit_price', 'profit']
+    temp = ret.iloc[:80].copy()
+    data_columns = ['time', 'entry_time', 'entry_price', 'exit_time', 'exit_price', 'profit', 'reason']
     trade_data = pd.DataFrame(columns=data_columns)
-    ret = ret[530:]
-    ret.reset_index(inplace=True)
+    ret = ret[80:]
     order_placed = False
     order_signal = False
     confirmation_waiting = False
     order_counter = 0
     bull_or_bear = None  
     stoploss = 0 
+    reason = "nothing"
     
     for i in range(0, len(ret)): 
         if (order_placed == True and order_signal == False and confirmation_waiting == False):
@@ -231,12 +251,22 @@ def main():
                 order_counter = order_counter + 1
         elif (order_placed == False and order_signal == False and confirmation_waiting == True):
             order_signal, confirmation_waiting, stoploss = confirmation(temp, bull_or_bear)
+            if order_signal == True:
+                reason = "came in waiting, placing order"
+            else:
+                reason = "came in wating, got caneled"
+            trade_data = append_value(trade_data, 'time', temp["time"].iloc[-1], order_counter)
+            trade_data = append_value(trade_data, 'reason', reason, order_counter)
+            order_counter = order_counter + 1
         elif (order_placed == False and order_signal == True and confirmation_waiting == False):
             entry_time , entry_price, order_signal, order_placed, stoploss = place_order(temp, bull_or_bear, stoploss)
             trade_data = append_value(trade_data, 'entry_time', entry_time, order_counter)
             trade_data = append_value(trade_data, 'entry_price', entry_price, order_counter)
         elif (order_placed == False and order_signal == False and confirmation_waiting ==  False):
-            order_signal, confirmation_waiting, bull_or_bear, stoploss = check_trade(temp)
+            order_signal, confirmation_waiting, bull_or_bear, stoploss, reason = check_trade(temp)
+            trade_data = append_value(trade_data, 'time', temp["time"].iloc[-1], order_counter)
+            trade_data = append_value(trade_data, 'reason', reason, order_counter)
+            order_counter = order_counter + 1
         
         
         if (i%50 == 0):
@@ -247,7 +277,7 @@ def main():
     
     current_directory = os.getcwd()
     df_comb_file = os.path.join(current_directory, 'testing4.csv')
-    trade_data.to_csv(df_comb_file, index=False)
+    trade_data.to_csv(df_comb_file, index=True)
     
     
 if __name__ == "__main__":
