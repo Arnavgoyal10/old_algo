286c85b580f700ce443e78c2abf62c5af4074990
Arnavgoyal10 <arnavgoyal63774@gmail.com>
2024-06-08 01:22:16 +0530

    betterment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from tvDatafeed import TvDatafeed, Interval
+# from tvDatafeed import TvDatafeed, Interval
 import pandas as pd
 import refracted_advance as refracted
 import cProfile
@@ -9,14 +9,14 @@ import current_indicators.squeeze as squeeze
 import current_indicators.impulsemacd as impulsemacd
 import current_indicators.tsi as tsi
 
-def trading_view():
-    tv = TvDatafeed(username='arnavgoyal63774', password='fAC@6kjug8tgqM-')
-    tv = TvDatafeed()
+# def trading_view():
+#     tv = TvDatafeed(username='arnavgoyal63774', password='fAC@6kjug8tgqM-')
+#     tv = TvDatafeed()
 
-    ret = tv.get_hist(symbol='BANKNIFTY',exchange='NSE',interval=Interval.in_1_minute,n_bars=20000)                                          
-    current_directory = os.getcwd()
-    df_comb_file = os.path.join(current_directory, 'bank_nifty_full1.csv')
-    ret.to_csv(df_comb_file, index=True)
+#     ret = tv.get_hist(symbol='BANKNIFTY',exchange='NSE',interval=Interval.in_1_minute,n_bars=20000)                                          
+#     current_directory = os.getcwd()
+#     df_comb_file = os.path.join(current_directory, 'bank_nifty_full1.csv')
+#     ret.to_csv(df_comb_file, index=True)
 
 ohlc=['into', 'inth', 'intl', 'intc']
      
@@ -60,8 +60,10 @@ def main():
         trade_columns = ['entry_time', 'entry_price', 'exit_time', 'exit_price', 'profit']
         trade_data = pd.DataFrame(columns=trade_columns)
         
-        hyper_params = [10.02,14.72,48.73,27,33.43,9,14.87,31.35,15.1]
-        parse = [2.87,2.12]
+        # hyper_params = [10.02,14.72,48.73,27,33.43,9,14.87,31.35,15.1]
+        # parse = [2.87,2.12]
+        hyper_params = [17,14,57,28,27,8,12,23,11]
+        parse = [15,6]
         df = calculate_indicators(ret, hyper_params)
         temp = df.iloc[:200].copy()
         df = df[200:]
@@ -79,9 +81,9 @@ def main():
     
     stats = pstats.Stats(pr)
     stats.sort_stats(pstats.SortKey.TIME)
-    stats.dump_stats("profile.prof")
+    stats.dump_stats("profile1.prof")
     
-    df_comb_file = os.path.join(base_directory, 'trade_data_new_test.csv')
+    df_comb_file = os.path.join(base_directory, 'trade_data_new_test1.csv')
     trade_data.to_csv(df_comb_file, index=True)
 
 
