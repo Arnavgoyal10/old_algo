8de44f1fe6b874ddbb2c8be75caf442263a214d4
Arnavgoyal10 <arnavgoyal63774@gmail.com>
2024-07-05 01:35:57 +0530

    done

--- final_2.py
+++ final_2.py
@@ -1,6 +1,8 @@
-import min1_agg.final1 as final1
-import min3_agg.final3 as final3
-import min5_agg.final5 as final5
+import trial_hull.final_hull as final1
+import trial_okx.final_okx as final2
+import trial_okxfinal.final_okxfinal as final3
+import trial_squee.final_squee as final4
+import trial_super.final_super as final5
 import threading
 
 
@@ -8,7 +10,7 @@ def run_module(module):
     module.main()
 
 def main():
-    modules = [final1, final3, final5]
+    modules = [final1, final2, final3, final4, final5]
     threads = []
     
     for module in modules:
