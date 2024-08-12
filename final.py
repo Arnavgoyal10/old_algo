af9868fd043b2417da90a80990184b1f9caed9f1
Arnavgoyal10 <arnavgoyal63774@gmail.com>
2024-08-13 04:26:51 +0530

    hour

--- final.py
+++ final.py
@@ -1,26 +1,27 @@
-import min3_prof.final3 as final3_prof
-import min5_prof.final5 as final5_prof
+import hour_prof.final3 as final3_prof
+import hour_prof.final3 as final5_prof
 import threading
 
 
 def run_module(module):
     module.main()
 
+
 def main():
     modules = [final3_prof, final5_prof]
-    
+
     print("TEST")
-    
+
     threads = []
-    
+
     for module in modules:
         thread = threading.Thread(target=run_module, args=(module,))
         thread.start()
         threads.append(thread)
-    
+
     for thread in threads:
         thread.join()
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
-    
